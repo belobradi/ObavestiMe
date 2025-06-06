9a40cf4694db0d38964c77aa699a4face0f5d876
Nemanja Stankic <stankic.nemanja@gmail.com>
2025-06-06 23:24:23 +0200

    Windows - CMake with vcpkg

--- setup.bat
+++ setup.bat
@@ -1,4 +1,3 @@
-git clone https://github.com/microsoft/vcpkg.git tools\vcpkg
-.\tools\vcpkg\bootstrap-vcpkg.bat
-tools\vcpkg\vcpkg integrate install
+.\vcpkg\bootstrap-vcpkg.bat
+.\vcpkg\vcpkg integrate install
 cmake --preset default
--- src/cppscraper/cppscraper.cpp
+++ src/cppscraper/cppscraper.cpp
@@ -1,10 +1,11 @@
 ﻿// cppscraper.cpp : Defines the entry point for the application.
 //
+#include <iostream>
 
 #include "cppscraper.h"
 #include "curl/curl.h"
 
-int main()
-{
+int main() {
+	std::cout << "Hello World!";
 	return 0;
 }

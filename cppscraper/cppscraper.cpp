4626104f2cd4536bb0bf66656e17b41460ace0e5
Nemanja Stankic <stankic.nemanja@gmail.com>
2025-07-06 18:31:50 +0200

    #5 Fetch HTML from Utility Website * Website link is created based on a map. * libcurl fetches HTML content.

--- cppscraper/cppscraper.cpp
+++ cppscraper/cppscraper.cpp
@@ -4,8 +4,50 @@
 
 #include "cppscraper.h"
 #include "curl/curl.h"
+#include <format>
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+// Callback function to write the response data into a std::string
+auto WriteCallback(void* contents, size_t size, size_t nmemb, void* userp) {
+	auto totalSize{ size * nmemb };
+	auto* html{ static_cast<std::string*>(userp) };
+	html->append(static_cast<char*>(contents), totalSize);
+	return totalSize;
+}
+
+auto GetHtmlContent(const std::string& url) {
+	std::string htmlContent;
+	const auto curl{ curl_easy_init() };
+	if (curl) {
+		curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &htmlContent);
+		curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+		const auto res{ curl_easy_perform(curl) };
+		if (res != CURLE_OK) {
+			std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res) << "\n";
+			htmlContent.clear();
+		}
+		curl_easy_cleanup(curl);
+	}
+	return htmlContent;
+}
+
+auto GetUrlString(const int distributivnoPodrucje) {
+	const auto& data{ urlData.at(distributivnoPodrucje) };
+	return "https://elektrodistribucija.rs/planirana-iskljucenja-" + data.first + "/" + data.second + "Dan_0_Iskljucenja.htm";
+}
 
 int main() {
-	std::cout << "Hello World!";
+#ifdef _WIN32
+	SetConsoleOutputCP(CP_UTF8); // Enable UTF-8 output in console
+#endif
+
+	const auto urlString{ GetUrlString(distributivnopodrucje::ED_BEOGRAD) };
+
+	const auto htmlContent{ GetHtmlContent(urlString) };
+	std::cout << htmlContent << "\n";
 	return 0;
 }
